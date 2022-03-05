93ff021d48849ed4057033bff9a67ac26e22cbda
anhduc1000 <nguyen.anh.duc@sun-asterisk.com>
2022-03-05 14:21:02 +0700

    change useEffect in videos

--- components/videos.js
+++ components/videos.js
@@ -2,23 +2,24 @@ import styles from './Videos.module.css'
 import React, { useState, useEffect } from 'react'
 import Navbar from "./navbar";
 
-export default function Videos() {
-  const [data, setData] = useState(null)
-  const [isLoading, setLoading] = useState(false)
+export default function Videos({ data }) {
+  const [link, setLink] = useState(null)
+  // const [isLoading, setLoading] = useState(false)
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
-    setLoading(true)
-    fetch(`http://localhost:3000/api/wiki`)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data.map(elem => elem.videoSrc))
-        setLoading(false)
-      })
+    //   setLoading(true)
+    //   fetch(`http://localhost:3000/api/wiki`)
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    // setData(data.map(elem => elem.videoSrc))
+    setLink(data.map(elem => elem.videoSrc))
+    // setLoading(false)
+    //     })
   }, [])
 
-  if (isLoading) return <p>Loading...</p>
-  if (!data) return <p>No profile data</p>
+  // if (isLoading) return <p>Loading...</p>
+  if (!link) return <p>No profile data</p>
 
   return (
     <>
@@ -31,7 +32,7 @@ export default function Videos() {
         </div>
         <div className={`${styles["television__center"]}`}>
           <div className={`${styles["television__screen"]}`}>
-            <iframe className={`${styles["iframe"]}`} src={data[index]} frameBorder="0" allowFullScreen muted></iframe>
+            <iframe className={`${styles["iframe"]}`} src={link[index]} frameBorder="0" allowFullScreen muted></iframe>
           </div>
           <div className={`${styles["television__channels-wrapper"]}`}>
             <div className={`${styles["television__button-next"]}`}><a onClick={() => setIndex(prevCount => prevCount + 1)} title="Channel 6">Next</a></div>
--- pages/cat-videos/index.js
+++ pages/cat-videos/index.js
@@ -1,11 +1,24 @@
 import Videos from '../../components/videos';
+import { getAllBlogData } from "../../lib/blogs";
+
+export function getServerSideProps() {
+  const data = getAllBlogData();
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: { data }, // will be passed to the page component as props
+  }
+}
 
 
-export default function CatVideos() {
-
+export default function CatVideos(props) {
+  const data = props.data;
   return (
     <>
-      <Videos />
+      <Videos data={data} />
     </>
   )
 }
