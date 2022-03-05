3a89fd228a4a00afe0cb4bef0776374a734c62f9
anhduc1000 <nguyen.anh.duc@sun-asterisk.com>
2022-03-05 11:50:12 +0700

    remove all path getting

--- pages/wi-kat/blog/[id].js
+++ pages/wi-kat/blog/[id].js
@@ -1,12 +1,10 @@
-import { getBlogData } from "../../../lib/blogs";
+import { getBlogData, getAllBlogData } from "../../../lib/blogs";
 import Navbar from "../../../components/navbar";
 import fs from "fs";
 
 
 export async function getStaticPaths() {
-  const rawData = fs.readFileSync("./breeds/breeds.json");
-  const blogs = JSON.parse(rawData);
-  console.log(blogs);
+  const blogs = getAllBlogData();
   if (!blogs) {
     return {
       notFound: true,
